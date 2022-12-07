2f72b9906ce8cd63328d7ab2c532f62bf4bf52e1
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-12-07 19:01:14 +0530

    uc3 insertion in table

---
+++ SQL_UC-3.sql
@@ -0,0 +1,24 @@
+----UC1---
+CREATE DATABASE EMP_PAYROLL; 
+use EMP_PAYROLL
+
+---UC2---
+CREATE TABLE EMPLOYEE_PAYROLL
+ (
+ Id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+ NAME VARCHAR(100) NOT NULL,
+ Salary FLOAT NOT NULL,
+ StartDate DATE NOT NULL,
+ );
+
+ ---UC3----
+ -------Inserting values into Employee_payroll table----------
+
+INSERT INTO employee_payroll VALUES('Raj','50000','12-july-2020');
+INSERT INTO employee_payroll VALUES('Shri','40000','12-jan-2010');
+INSERT INTO employee_payroll VALUES('Priyanshu','15000','20-mar-2018');
+INSERT INTO employee_payroll VALUES('Sachin','10000','15-FEB-2010');
+INSERT INTO employee_payroll VALUES('Nikita','50000','13-apr-2025');
+
+
+
